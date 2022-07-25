3904e9e0b593653224ab07ccc70ac4c98213e09e
Keven C <keven_jkc@hotmail.com>
2022-07-25 15:17:46 -0300

    verificação com usuário inserido na tabela

--- sqlalchemy/env/app.py
+++ sqlalchemy/env/app.py
@@ -2,26 +2,31 @@ import re
 from urllib import response
 from flask import Flask 
 from flask_restful import Resource, Api, request
-from models import Pessoas
+from models import Pessoas, Atividades, Usuarios
 from flask_httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
 app = Flask(__name__)
 api =  Api(app)
 
-USUARIOS = {
-    'keven': '123',
-    'cris':'321'
-}
+# USUARIOS = {
+#     'keven': '123',
+#     'cris':'321'
+# }
 
 #Decorador
+#Autenticação com hard code
+# @auth.verify_password
+# def verificacao(login, senha):
+#     if not(login, senha):
+#         return False
+#     return USUARIOS.get(login) == senha
+
 @auth.verify_password
 def verificacao(login, senha):
-    print('Validando usuario')
-    print(USUARIOS.get(login) == senha)
     if not(login, senha):
         return False
-    return USUARIOS.get(login) == senha
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
 
 
 
@@ -65,6 +70,7 @@ class Pessoa(Resource):
         return {'status': 'success', 'mensagem':response}
 
 class ListaPessoas(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
         response = [{'id': i.id, 'nome': i.nome, 'idade': i.idade} for i in pessoas]
--- sqlalchemy/env/utils.py
+++ sqlalchemy/env/utils.py
@@ -1,4 +1,5 @@
-from models import Pessoas
+from re import U
+from models import Pessoas, Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Juarez', idade=34)
@@ -20,9 +21,19 @@ def excluir_pessoa():
     pessoa = Pessoas.query.filter_by(nome='azulão').first()
     pessoa.delete()
     
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_usuario():
+    usuario = Usuarios.query.all()
+    print(usuario)
 
 if __name__ == '__main__':
-    insere_pessoas()
+    insere_usuario('Saulo','2345')
+    insere_usuario('Felipe','8762')
+    consulta_usuario()
+    # insere_pessoas()
     # # altera_pessoa()
     # excluir_pessoa()
-    consulta_pessoa()
+    # consulta_pessoa()
